2047e5e64e4b2427650ef69bd6d22cd997d23ab6
Jeff Wang <wohaaitinciu@163.com>
2015-06-07 14:52:08 +0800

    测试代码

---
+++ Classes/tests/NotificationCenterTest.cpp
@@ -0,0 +1,35 @@
+﻿#include "NotificationCenterTest.h"
+#include "../wheels/CWNotificationCenter.h"
+
+USING_NS_CC;
+
+bool NotificationCenterTestLayer::init()
+{
+    if (!Layer::init())
+    {
+        return false;
+    }
+
+    cw::NotificationCenter::getInstance()->addObserver(this, [](Ref *r) {
+        CCLOG("%s %d", __FUNCTION__, __LINE__);
+    }, "test1", nullptr);
+
+    cw::NotificationCenter::getInstance()->addObserver(this, [](Ref *r) {
+        CCLOG("%s %d", __FUNCTION__, __LINE__);
+    }, "test2", nullptr);
+
+    cw::NotificationCenter::getInstance()->addObserver(this, [](Ref *r) {
+        CCLOG("%s %d", __FUNCTION__, __LINE__);
+    }, "test2", nullptr);
+
+    cw::NotificationCenter::getInstance()->postNotification("test1");
+    cw::NotificationCenter::getInstance()->removeObserver(this, "test1");
+    cw::NotificationCenter::getInstance()->postNotification("test1");
+    cw::NotificationCenter::getInstance()->postNotification("test2");
+
+    cw::NotificationCenter::getInstance()->removeAllObservers(this);
+    cw::NotificationCenter::getInstance()->postNotification("test1");
+    cw::NotificationCenter::getInstance()->postNotification("test2");
+
+    return true;
+}
---
+++ Classes/tests/NotificationCenterTest.h
@@ -0,0 +1,12 @@
+﻿#ifndef _NOTIFICATION_CENTER_TEST_H_
+#define _NOTIFICATION_CENTER_TEST_H_
+
+#include "cocos2d.h"
+
+class NotificationCenterTestLayer : public cocos2d::Layer {
+public:
+    CREATE_FUNC(NotificationCenterTestLayer);
+    virtual bool init() override;
+};
+
+#endif
