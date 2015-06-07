810823e4b0e1e20681d653ed85dce0c681777574
Jeff Wang <wohaaitinciu@163.com>
2015-06-07 14:49:06 +0800

    使得2.x版本可以添加std::function作为notification的回调

---
+++ Classes/wheels/CWNotificationCenter.cpp
@@ -0,0 +1,78 @@
+﻿#include "CWNotificationCenter.h"
+#include <algorithm>
+
+USING_NS_CC;
+
+namespace cw {
+
+    NotificationCenter *NotificationCenter::_this = nullptr;
+
+    void NotificationCenter::addObserver(void *target, const std::function<void (cocos2d::Ref *)> &selector, const std::string &name, cocos2d::Ref *sender)
+    {
+        std::unordered_map<void *, CallFuncOMap>::iterator it = _targetMap.find(target);
+        if (it != _targetMap.end())
+        {
+            CallFuncOMap &callfuncOMap = it->second;
+            CallFuncOMap::iterator it0 = callfuncOMap.find(name);
+            if (it0 != callfuncOMap.end())
+            {
+                _nc->removeObserver(it0->second.get(), name);
+
+                CallFuncODelegate *ft = CallFuncODelegate::create(selector);
+                it0->second = AutoRef<CallFuncODelegate>(ft);
+                _nc->addObserver(ft, CallFuncODelegate::getSelector(), name, sender);
+            }
+            else
+            {
+                CallFuncODelegate *ft = CallFuncODelegate::create(selector);
+                callfuncOMap.insert(std::make_pair(name, AutoRef<CallFuncODelegate>(ft)));
+                _nc->addObserver(ft, CallFuncODelegate::getSelector(), name, sender);
+            }
+        }
+        else
+        {
+            CallFuncOMap callfuncOMap;
+            CallFuncODelegate *ft = CallFuncODelegate::create(selector);
+            callfuncOMap.insert(std::make_pair(name, AutoRef<CallFuncODelegate>(ft)));
+            _targetMap.insert(std::make_pair(target, std::move(callfuncOMap)));
+            _nc->addObserver(ft, CallFuncODelegate::getSelector(), name, sender);
+        }
+    }
+
+    void NotificationCenter::removeObserver(void *target, const std::string &name)
+    {
+        std::unordered_map<void *, CallFuncOMap>::iterator it = _targetMap.find(target);
+        if (it != _targetMap.end())
+        {
+            CallFuncOMap &callfuncOMap = it->second;
+            CallFuncOMap::iterator it0 = callfuncOMap.find(name);
+            if (it0 != callfuncOMap.end())
+            {
+                _nc->removeObserver(it0->second.get(), name);
+                callfuncOMap.erase(it0);
+
+                if (callfuncOMap.empty())
+                {
+                    _targetMap.erase(it);
+                }
+            }
+        }
+    }
+
+    int NotificationCenter::removeAllObservers(void *target)
+    {
+        int cnt = 0;
+        std::unordered_map<void *, CallFuncOMap>::iterator it = _targetMap.find(target);
+        if (it != _targetMap.end())
+        {
+            CallFuncOMap &callfuncOMap = it->second;
+            std::for_each(callfuncOMap.begin(), callfuncOMap.end(), [this, &cnt](CallFuncOMap::value_type &pair) {
+                _nc->removeObserver(pair.second.get(), pair.first);
+                ++cnt;
+            });
+            _targetMap.erase(it);
+        }
+        return cnt;
+    }
+
+}
