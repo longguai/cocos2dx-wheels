a61af4585af68e10429a4c82514c35d1e555b65b
Jeff Wang <wohaaitinciu@163.com>
2015-06-07 18:19:34 +0800

    更多测试

--- Classes/tests/NotificationCenterTest.cpp
+++ Classes/tests/NotificationCenterTest.cpp
@@ -1,5 +1,6 @@
 ﻿#include "NotificationCenterTest.h"
 #include "../wheels/CWNotificationCenter.h"
+#include "../wheels/CWRefConverter.h"
 
 USING_NS_CC;
 
@@ -19,13 +20,19 @@ bool NotificationCenterTestLayer::init()
     }, "test2", nullptr);
 
     cw::NotificationCenter::getInstance()->addObserver(this, [](Ref *r) {
-        CCLOG("%s %d", __FUNCTION__, __LINE__);
-    }, "test2", nullptr);
+        CCLOG("%s %d %d", __FUNCTION__, __LINE__, ((cw::RefConverter<int> *)r)->get());
+    }, "test2", cw::createRefConverter(100));
 
     cw::NotificationCenter::getInstance()->postNotification("test1");
     cw::NotificationCenter::getInstance()->removeObserver(this, "test1");
     cw::NotificationCenter::getInstance()->postNotification("test1");
     cw::NotificationCenter::getInstance()->postNotification("test2");
+
+    cw::NotificationCenter::getInstance()->removeObserver(this, "test2");
+    cw::NotificationCenter::getInstance()->addObserver(this, [](Ref *r) {
+        CCLOG("%s %d %d", __FUNCTION__, __LINE__, ((cw::RefConverter<int> *)r)->get());
+    }, "test2", nullptr);
+    cw::NotificationCenter::getInstance()->postNotification("test2", cw::createRefConverter(1));
 
     cw::NotificationCenter::getInstance()->removeAllObservers(this);
     cw::NotificationCenter::getInstance()->postNotification("test1");
---
+++ Classes/wheels/CWRefConverter.h
@@ -0,0 +1,57 @@
+﻿#ifndef _CW_REF_CONVERTER_H_
+#define _CW_REF_CONVERTER_H_
+
+#include "base/CCRef.h"
+
+namespace cw {
+
+    template <class _T> class RefConverter : public cocos2d::Ref {
+        _T _val;
+    public:
+        static RefConverter<_T> *create(const _T &val) {
+            RefConverter<_T> *ret = new (std::nothrow) RefConverter<_T>();
+            if (ret != nullptr) {
+                ret->_val = val;
+                ret->autorelease();
+                return ret;
+            }
+            return nullptr;
+        }
+
+        static RefConverter<_T> *create(_T &&val) {
+            RefConverter<_T> *ret = new (std::nothrow) RefConverter<_T>();
+            if (ret != nullptr) {
+                ret->_val = std::move(val);
+                ret->autorelease();
+                return ret;
+            }
+            return nullptr;
+        }
+
+        inline _T &get() {
+            return _val;
+        }
+
+        inline const _T &get() const {
+            return _val;
+        }
+
+        inline void reset(const _T &val) {
+            _val = val;
+        }
+
+        inline void reset(_T &&val) {
+            _val = std::move(val);
+        }
+    };
+
+    template <class _T> RefConverter<_T> *createRefConverter(const _T &val) {
+        return RefConverter<_T>::create(val);
+    }
+
+    template <class _T> RefConverter<_T> *createRefConverter(_T &&val) {
+        return RefConverter<_T>::create(std::move(val));
+    }
+}
+
+#endif
