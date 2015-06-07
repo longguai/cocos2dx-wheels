9f2973af739eec3584c697bb289371368c336225
Jeff Wang <wohaaitinciu@163.com>
2015-06-07 22:23:59 +0800

    整理总的头文件

--- Classes/wheels/cocos2dx-wheels.h
+++ Classes/wheels/cocos2dx-wheels.h
@@ -1,16 +1,19 @@
 ﻿#ifndef _COCOS2DX_WHEELS_H_
 #define _COCOS2DX_WHEELS_H_
 
+#include "CWCallFuncDelegate.h"
+#include "CWCoordinateSystem.h"
+#include "CWDebugHelper.h"
+#include "CWGeometry.h"
+#include "CWLayoutFunctions.h"
+#include "CWMacros.h"
+#include "CWNotificationCenter.h"
+#include "CWRefConverter.h"
+#include "CWRefWrap.h"
 #include "CWRichTextEx.h"
 #include "CWScrollBar.h"
 #include "CWTableView.h"
-#include "CWGeometry.h"
-#include "CWLayoutFunctions.h"
-#include "CWDebugHelper.h"
 #include "CWVariableArgumentList.h"
-
-#include "base/CCVector.h"
-#include "2d/CCActionInterval.h"
 
 namespace cw {
     // 创建与ScrollView关联的ScrollBar
